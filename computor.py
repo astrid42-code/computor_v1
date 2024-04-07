cc309294079e8fe8931d4220adced855f4fa189c
Astrid Gaultier <pingu007@c1s4d15.42wolfsburg.de>
2024-04-07 15:23:13 +0200

    avancee du parsing

--- computor.py
+++ computor.py
@@ -1,14 +1,18 @@
-import sys
+import sys, re
 
-def check_arg(ac: int):
+def check_arg(ac: int, av: str):
     if ac != 2:
-        print("Wrong number of argments")
+        print("Wrong number of arguments")
+        exit()
+    c = av.count("=") # faire une variable dans une classe pour mettre a true ou false la validite de l equation?
+    if c != 1:
+        print("There must be at least one equal sign, no more, no less!")
         exit()
     else:
         print("Let's have some fun with polynomial equations!")
 
 
-def special_cases(str1: str, str2: str)-> bool, int:
+def special_cases(str1: str, str2: str): #-> bool, int:
     # prevoir un tableau genre switch ou un dico pour les msgs d erreur 
     if str1 == str2:
         print('c tout pareil')
@@ -19,7 +23,11 @@ def special_cases(str1: str, str2: str)-> bool, int:
 
 def parser(str1: str, str2: str):
     print(str1, str2)
-    # voir exemple du sujet (meme chose des 2 cotes) et trouver les differentes exceptions possibles
+    
+    # 1 verifier et stocker les elements de str1 et 2
+
+
+    # 2 special_cases : voir exemple du sujet (meme chose des 2 cotes) et trouver les differentes exceptions possibles
     if special_cases(str1, str2) == False:
         # print le cas d erreur en fct du type d erreur (mettre un nombre qui renvoie a un msg d erreur type) 
         exit()
@@ -30,14 +38,21 @@ def main():
     av = sys.argv
     ac = len(av)
 
-    check_arg(ac)
+    check_arg(ac, av[1])
     
     # avant parsing : 
     # - découper la str pour avoir les differents éléments de l expression dans l ordre
     # + faire une str1 avec les éléments avant '=' et une str2 avec ceux après
     
     res = av[1].split('=')
+    res[0] = re.sub(" ", "", res[0])
+    res[1] = re.sub(" ", "", res[1])
     str1, str2 = res[0], res[1]
+
+    if len(str1) == 0 or len(str2) == 0 :
+        print(" You need element on both side of equal sign")
+        exit()
+
     # > une methode pour trouver directement str1 et 2 sans passer par res (= faire le split direct) ?
 
     parser(str1, str2)
