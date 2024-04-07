891d1b9c090db486e896359bf0d734a0fa7e6956
Astrid Gaultier <pingu007@c1s4d15.42wolfsburg.de>
2024-04-07 16:03:47 +0200

    avancee du parsing 2

--- computor.py
+++ computor.py
@@ -12,23 +12,38 @@ def check_arg(ac: int, av: str):
         print("Let's have some fun with polynomial equations!")
 
 
-def special_cases(str1: str, str2: str): #-> bool, int:
+def special_cases(left: str, right: str): #-> bool, int:
     # prevoir un tableau genre switch ou un dico pour les msgs d erreur 
-    if str1 == str2:
+    if left == right:
         print('c tout pareil')
         # cf exemple du sujet et trouver comment ca se gere (ex ici : tous les nombres réels sont une solution)
         return (False, 0)
 
     return (True, 1)
 
-def parser(str1: str, str2: str):
-    print(str1, str2)
+def parser(left: str, right: str):
+    print(left, right)
     
-    # 1 verifier et stocker les elements de str1 et 2
+    # 1 verifier et stocker les elements de left et right
+    d1, d2 = {}, {}
+    
+    # right part
+    res = right.split('*')
+    
+    d2[res[0]] = res[1][2:]
+
+    print(res, d2)
+
+    mais comment ca se passe quand plsrs elements en right (a faire avec left )
+
+    + verifier que chaque membre existe bien (ex la cle ou la value ne sont pas nulles) sinon msg erreur et exit 
+    > check qu il y a bien X^, et les valeurs
+    > a faire dans une boucle ou - et + sont a conserver (enfin surtout le - s il y a !!) 
+    et servent de separateurs pour remplir le dico et verifier que tous les elements sont bien la dans chaque morceau
 
 
     # 2 special_cases : voir exemple du sujet (meme chose des 2 cotes) et trouver les differentes exceptions possibles
-    if special_cases(str1, str2) == False:
+    if special_cases(left, right) == False:
         # print le cas d erreur en fct du type d erreur (mettre un nombre qui renvoie a un msg d erreur type) 
         exit()
     # sinon trouver la puissance du cote droit et le soustraire de celui a puissance équivalente cote gauche
@@ -42,20 +57,20 @@ def main():
     
     # avant parsing : 
     # - découper la str pour avoir les differents éléments de l expression dans l ordre
-    # + faire une str1 avec les éléments avant '=' et une str2 avec ceux après
+    # + faire une string left avec les éléments avant '=' et une string right avec ceux après
     
     res = av[1].split('=')
     res[0] = re.sub(" ", "", res[0])
     res[1] = re.sub(" ", "", res[1])
-    str1, str2 = res[0], res[1]
+    left, right = res[0], res[1]
 
-    if len(str1) == 0 or len(str2) == 0 :
+    if len(left) == 0 or len(right) == 0 :
         print(" You need element on both side of equal sign")
         exit()
 
-    # > une methode pour trouver directement str1 et 2 sans passer par res (= faire le split direct) ?
+    # > une methode pour trouver directement left et right sans passer par res (= faire le split direct) ?
 
-    parser(str1, str2)
+    parser(left, right)
 
 
 if __name__ == "__main__":
