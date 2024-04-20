2cf0053a362dbdd7bf6bee5a35b1bf2b21aa0fa3
Astrid Gaultier <pingu007@c2s1d1.42wolfsburg.de>
2024-04-20 18:39:17 +0200

    resolution equations 1er degre; reste le cas 1 qui pose souci

--- computorv1.py
+++ computorv1.py
@@ -24,27 +24,30 @@ def parse(av: str):
     # les garder dans une liste
     for i in range(len(res)):
         # Séparation de la chaîne en fonction de '*'
-        c = res[i].split('*')
-        if (len(c) > 1):
-            if c[0][0] == '+':
-                c = c[0][1::]
-                res[i] = float(c)
+        coeff = res[i].split('*')
+        if (len(coeff) > 1):
+            if coeff[0][0] == '+':
+                coeff = coeff[0][1::]
+                res[i] = float(coeff)
             # Remplacement de l'élément par la partie avant '*'
             else:
-                res[i] = float(c[0])
+                res[i] = float(coeff[0])
 
     # substract 1st term and last term :
     # res[0] -= res[len(res)-1] # possible, mais necessite ensuite de remove le dernier element de la liste
-    res[0] -= res.pop() #  pour recuperer le dernier element de la liste et le retirer directement
+    # print(len(res), res.pop())
+    if len(res) > 2:
+        res[0] -= res.pop() #  pour recuperer le dernier element de la liste et le retirer directement
 
     # Souci a regler : si 1er term ou dernier sont negatifs
-    
+    # + quand 1 seul terme de chaque cote (il ne veut pas soustraire element 0 et 1)
+
     # retransformer av avec 1er terme = c[0] mais pb des float/int > trouver la fct qui trouve si c int (x.0) ou float (x.8)
-    c = 0
+    count = 0
     for i in res:
         if i.is_integer():
-            res[c] = int(i)
-        c += 1
+            res[count] = int(i)
+        count += 1
     
     red = av[av.find('*'):av.find('=')]
     # puis recopier tout ce qui est a partir du '*' jusqu au '='
@@ -56,23 +59,27 @@ def parse(av: str):
 
 def result(res):
     # faire 4 cas en fct du degre : 0, 1, 2, tout le reste
-    if len(res) == 1:
-        print('1')
-    elif len(res) == 2:
-        print('2')
-    elif len(res) == 3:
-        # https://www.maths-et-tiques.fr/telech/20Poly.pdf
-        a = res[2]
+    a = res[0]
+    if len(res) == 1: # http://serge.mehl.free.fr/anx/equ1.html
+        if a == 0:
+            print('There is infinite solutions')
+        else:
+            print('There is no solution')
+    elif len(res) == 2: # 1st degree : http://serge.mehl.free.fr/anx/equ1.html
         b = res[1]
-        c = res[0]
-        discriminant = (b ** 2) - (4 * a * c)
+        z = -a / b
+        print('The solution is :\n', z)
+    elif len(res) == 3: # 2nd degree
+        c = res[2]
+        # https://www.maths-et-tiques.fr/telech/20Poly.pdf
+        discriminant = (b ** 2) - (4 * c * a)
         # print(discriminant)
         if discriminant > 0:
-            z1 = (-b - math.sqrt(discriminant)) / (2 * a) # z1 = -b-√discriminant / 2a
-            z2 = (-b + math.sqrt(discriminant)) / (2 * a) # z2 = -b+√discriminant / 2a
+            z1 = (-b - math.sqrt(discriminant)) / (2 * c) # z1 = -b-√discriminant / 2a
+            z2 = (-b + math.sqrt(discriminant)) / (2 * c) # z2 = -b+√discriminant / 2a
             print('Discriminant is strictly positive, the two solutions are:\n', z1, '\n', z2)
         elif discriminant == 0:
-            z0 = -(b / (2 * a))
+            z0 = -(b / (2 * c))
             print('The solution is:\n', z0)
         elif discriminant < 0:
             print('No real solution, discriminant is strictly negative :', discriminant)
@@ -90,9 +97,6 @@ def main():
         exit()
 
     check_arg(ac, av[1])
-    
-    # avant parsing : 
-    # - découper la str pour avoir les differents éléments de l expression dans l ordre
 
     res=parse(av[1])
 
